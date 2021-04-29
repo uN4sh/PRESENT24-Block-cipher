c9ca9882a5918352ce3c5696568cfca0e398c25d
vdElyn <vdelyn@protonmail.com>
2021-04-29 15:28:31 +0200

    Optimisations: Substitution et Permutation bitwise -> ~1m/s

--- chiffrement.c
+++ chiffrement.c
@@ -7,6 +7,10 @@
 #define TAILLE_MOT 6
 #define TAILLE_MSG 24
 
+int p[TAILLE_MSG] = { 0, 6, 12, 18, 1, 7, 13, 19, 2, 8, 14, 20, 
+                      3, 9, 15, 21, 4, 10, 16, 22, 5, 11, 17, 23 };
+int s[16] = {12, 5, 6, 11, 9, 0, 10, 13, 3, 14, 15, 8, 4, 7, 1, 2};
+
 /**
  * @brief Convertit un entier décimal en entrée en chaîne binaire
  * 
@@ -40,8 +44,8 @@ void hexa_to_binary(char *hex, char *res)  {
  * 
  * @param bits  Chaîne de 4 bits à substituer selon la Boîte-S
  */
-void substitution_4_bits(char *bits)  {
-    int s[16] = {12, 5, 6, 11, 9, 0, 10, 13, 3, 14, 15, 8, 4, 7, 1, 2};
+void substitution_4_bits(char *bits, int *s)  {
+    // int s[16] = {12, 5, 6, 11, 9, 0, 10, 13, 3, 14, 15, 8, 4, 7, 1, 2};
 
     // Convert binary string to int
     int decvalue = strtol( bits, NULL, 2 );
@@ -60,7 +64,7 @@ void substitution_4_bits(char *bits)  {
  * x    0 1 2 3 4 5 6 7 8 9 a b c d e f
  * S[x] c 5 6 b 9 0 a d 3 e f 8 4 7 1 2
  */ 
-void SUBSTITUTION(char *etat)  {
+void SUBSTITUTION(char *etat, int *s)  {
     char substr[4];
     for (size_t i = 0; i < TAILLE_MSG; i+=4)  {
         // Récupération des 4 bits
@@ -68,14 +72,13 @@ void SUBSTITUTION(char *etat)  {
             substr[j] = etat[i+j];
         
         // Substitution des 4 bits selon la Boite-S
-        substitution_4_bits(substr);
+        substitution_4_bits(substr, s);
 
         // Replace in etat[] 
         for (size_t j = 0; j < 4; j++)
             etat[i+j] = substr[j];
     }
 }
-
 
 /**
  * @brief Couche linéaire du chiffrement : permutation bit-à-bit selon la table P.
@@ -86,9 +89,7 @@ void SUBSTITUTION(char *etat)  {
  * @param etat  Registre de 24 bits contenant l'état (chaine binaire)
  * @return      La variable état est modifiée, permuté bit-à-bit 
  */ 
-void PERMUTATION(char *etat)  {
-    int p[TAILLE_MSG] = { 0, 6, 12, 18, 1, 7, 13, 19, 2, 8, 14, 20, 
-                        3, 9, 15, 21, 4, 10, 16, 22, 5, 11, 17, 23 };
+void PERMUTATION(char *etat, int *p)  {
     
     char tmp[TAILLE_MSG+1];
     for (size_t i = 0; i < TAILLE_MSG; i++)  {
@@ -101,6 +102,53 @@ void PERMUTATION(char *etat)  {
     }
 }
 
+
+uint32_t SUBSTITUTION_OPTI(uint32_t etat, int *s)  {
+    // Step 2: Application de la Boîte-S aux bits 4 à 4 
+
+    uint32_t tmp;
+    tmp = etat >> 20; // Extraire les 4 premiers bits de high
+    etat = etat & 0x0FFFFF; // Passer les 4 premiers bits de High à 0
+    etat = etat | ((uint64_t)s[tmp] << 20); // Remplacer les 4 premiers bits par tmp
+
+    // Extraire les 4 deuxièmes :
+    tmp = (etat >> 16) & 0x0F;
+    etat = etat & 0xF0FFFF;
+    etat = etat | ((uint64_t)s[tmp] << 16);
+    
+    // Extraire les 4 3eme
+    tmp = (etat >> 12) & 0x00F;
+    etat = etat & 0xFF0FFF;
+    etat = etat | ((uint64_t)s[tmp] << 12);
+
+    // Extraire les 4 4eme
+    tmp = (etat >> 8) & 0x000F;
+    etat = etat & 0xFFF0FF;
+    etat = etat | ((uint64_t)s[tmp] << 8);
+
+     // Extraire les 4 5eme
+    tmp = (etat >> 4) & 0x0000F;
+    etat = etat & 0xFFFF0F;
+    etat = etat | ((uint64_t)s[tmp] << 4);
+
+    // Extraire les 4 derniers
+    tmp = etat & 0x00000F;
+    etat = etat & 0xFFFFF0;
+    etat = etat | s[tmp];
+
+    return etat;
+}
+
+uint32_t PERMUTATION_OPTI(uint32_t etat, int *p)  {
+    // À expliquer
+    uint64_t permutation = 0;
+    int i;
+    for (i=0; i<24; i++){
+        int distance = 23 - i;
+        permutation = permutation | ((etat >> distance & 0x1) << 23 - p[i]);
+    }
+    return permutation;
+}
 
 /**
  * @brief Cadencement de clé : génère 11 sous-clés de 24 bits chacune à partir d'une clé maître
@@ -161,7 +209,7 @@ void cadencement_cle(char *cle_maitre, char sous_cles[][25])  {
             substr_s2[j] = K[j];
         
         substr_s2[k] = '\0';
-        substitution_4_bits(substr_s2);
+        substitution_4_bits(substr_s2, s);
         
         for (size_t j = 0; j < 4; j++)
             K[j] = substr_s2[j];
@@ -189,38 +237,26 @@ void cadencement_cle(char *cle_maitre, char sous_cles[][25])  {
     // }
 }
 
-void cadencement_cle_new(char *m_key, uint32_t *sous_cles)  {
-    uint32_t master_key = strtol(m_key, NULL, 16); 
+void CADENCEMENT_CLE_OPTI(uint32_t m_key, uint32_t *sous_cles)  {
     // Division en High (bits 79-40) et Low (bits 39-16)
-    uint64_t master_high = (uint64_t)master_key << 16 ; // ajouter 16 0 sur la droite 
+    uint64_t master_high = (uint64_t)m_key << 16 ; // ajouter 16 0 sur la droite 
     uint64_t master_low = 0; /* 40 0 */
-    
-    uint64_t tmp;
-    uint64_t premiers_bits_de_low;
-    uint64_t premiers_bits_de_high;
-    uint64_t derniers_bits_de_low;
-    uint64_t derniers_bits_de_high;
-
-    int s[16] = {12, 5, 6, 11, 9, 0, 10, 13, 3, 14, 15, 8, 4, 7, 1, 2};
+    uint64_t tmp; // Tmp pour les rotations et extractions
 
     for (int i = 0; i < 11; i++)  {
         // Extraction de la clé
         sous_cles[i] = master_low >> 16; // Extraction bit 39 à 16 -> 16 premiers bits de Low
         
         // Step 1: Rotation du registre de 61 bits vers la gauche -> [k18k17..k0k79..k20k19]
-        premiers_bits_de_low = master_low >> 19;
-        premiers_bits_de_high = master_high >> 19;
-        derniers_bits_de_low = master_low & 0x7FFFF; // Masque avec 00...001111111111111111111
-        derniers_bits_de_high = master_high & 0x7FFFF;
+        tmp = master_high;
+        // Pour récupérer les 19 derniers bits: Masque & avec 00...001111111111111111111
+        master_high = ((master_low & 0x7FFFF) << 21) ^ (master_high >> 19); // 19 derniers bits de Low (18-0)    + 21 premiers bits de high (79-59)
+        master_low  = ((tmp        & 0x7FFFF) << 21) ^ (master_low  >> 19); // 19 derniers bits de high (58-40)  + 21 premiers bits de low  (39-19)
         
-        master_high = (derniers_bits_de_low << 21) ^ premiers_bits_de_high; // 19 derniers bits de Low (18-0)    + 21 premiers bits de high (79-59)
-        master_low  = (derniers_bits_de_high << 21) ^ premiers_bits_de_low; // 19 derniers bits de high (58-40)  + 21 premiers bits de low  (39-19)
-
         // Step 2: Application de la Boîte-S aux bits 79-76
         tmp = master_high >> 36; // Extraire les 4 premiers bits de high
-        tmp = s[tmp]; // Subsitution Boîte-S
         master_high = master_high & 0x0FFFFFFFFF; // Passer les 4 premiers bits de High à 0
-        master_high = master_high | ((uint64_t)tmp << 36); // Remplacer les 4 premiers bits par tmp
+        master_high = master_high | ((uint64_t)s[tmp] << 36); // Remplacer les 4 premiers bits par tmp substutué par boite-S
 
         // Step 3: XOR des bits extraits 19-15 avec la valeur binaire de i ∈ [1,11]
         master_low = master_low ^ (((uint64_t)i+1) << 15); // XOR des bits 19-15 avec i (shifté de 15)  
@@ -253,8 +289,8 @@ int CHIFFREMENT(char *etat_hex, char *cle_maitre_hex, char *cipher)  {
         result = strtol(etat, NULL, 2) ^ strtol(sous_cles[i], NULL, 2);
         decimal_to_binary(result, etat, 25);
         // Substitution et permutation de l'état
-        SUBSTITUTION(etat);
-        PERMUTATION(etat);
+        SUBSTITUTION(etat, s);
+        PERMUTATION(etat, p);
     }
 
     // Etat ← Etat ⊕ K_11
@@ -265,31 +301,21 @@ int CHIFFREMENT(char *etat_hex, char *cle_maitre_hex, char *cipher)  {
     return result;
 }
 
-int CHIFFREMENT2(char *etat_hex, char *cle_maitre_hex, char *cipher)  {
-    // Convertit etat_hex en chaîne binaire
-    char etat[TAILLE_MSG+1];
-    hexa_to_binary(etat_hex, etat);
-    
-    // Génération des 11 sous clés de 24 bits chacune via algo. cadencement de clés
-    uint32_t sous_cles[11];
-    cadencement_cle_new(cle_maitre_hex, sous_cles);
-    
-    int result;
+int CHIFFREMENT_OPTI(uint32_t etat, uint32_t *sous_cles)  {
     for (size_t i = 0; i < 10; i++)  {
         // Etat ← Etat ⊕ K_i
-        result = strtol(etat, NULL, 2) ^ sous_cles[i];
-        decimal_to_binary(result, etat, 25);
+        etat = etat ^ sous_cles[i];
         // Substitution et permutation de l'état
-        SUBSTITUTION(etat);
-        PERMUTATION(etat);
+        etat = SUBSTITUTION_OPTI(etat, s);
+        // decimal_to_binary(etat, tmp, 25);
+        etat = PERMUTATION_OPTI(etat, p);
     }
 
     // Etat ← Etat ⊕ K_11
-    result = strtol(etat, NULL, 2) ^ sous_cles[10];
+    etat = etat ^ sous_cles[10];
 
     // Message chiffré retourné après 10 tours d'algorithme
-    snprintf ( cipher, TAILLE_MOT+1, "%06x", result);
-    return result;
+    return etat;
 }
 
 
@@ -304,4 +330,13 @@ int CHIFFREMENT2(char *etat_hex, char *cle_maitre_hex, char *cipher)  {
 int CHIFFREMENT_DOUBLE(char *message, char *cle_k1, char *cle_k2, char *cipher)  {
     CHIFFREMENT(message, cle_k1, cipher);
     return CHIFFREMENT(cipher, cle_k2, cipher);
+}
+
+int CHIFFREMENT_DOUBLE_OPTI(uint32_t message, uint32_t k1, uint32_t k2)  {
+    uint32_t sous_cles_k1[11];
+    uint32_t sous_cles_k2[11];
+    CADENCEMENT_CLE_OPTI(k1, sous_cles_k1);
+    CADENCEMENT_CLE_OPTI(k2, sous_cles_k2);
+    int res;
+    return CHIFFREMENT_OPTI(CHIFFREMENT_OPTI(message, sous_cles_k1), sous_cles_k2);
 }
